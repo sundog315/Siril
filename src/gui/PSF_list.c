5df5b06fcac9a1a488e2e69c83f3877892691f8b
lock <lock@0131fb5d-c1a1-4c56-9934-cd381e373ad5>
2016-03-31 08:41:41 +0000

    Fixing crash in RGB compositing when "clear button" was clicked after alignment. --> double free

--- src/gui/PSF_list.c
+++ src/gui/PSF_list.c
@@ -119,7 +119,7 @@ void clear_stars_list() {
 			/* freeing found stars. It must not be done when the only star in
 			 * com.stars is the same as com.seq.imgparam[xxx].fwhm, as set in
 			 * set_fwhm_star_as_star_list(), because it will be reused */
-			if (com.stars[1] || !sequence_is_loaded() || !com.star_is_seqdata) {
+			if (com.stars[1] || !com.star_is_seqdata) {
 				int i = 0;
 				while (i < MAX_STARS && com.stars[i])
 					free(com.stars[i++]);
