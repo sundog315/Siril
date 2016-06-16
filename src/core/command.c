d55ac7639b5e6edf9af21f8e64f49237c8342fe3
lock <lock@0131fb5d-c1a1-4c56-9934-cd381e373ad5>
2016-06-16 08:58:22 +0000

    Fixing warning on freebsd

--- src/core/command.c
+++ src/core/command.c
@@ -18,6 +18,10 @@
  * along with Siril. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include <unistd.h>
 #include <ctype.h>
