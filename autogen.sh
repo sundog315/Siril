bfcd452719e4f335fbf3e85972f5dc7ef4214822
lock <lock@0131fb5d-c1a1-4c56-9934-cd381e373ad5>
2016-06-23 09:27:10 +0000

    Adding new installation script: autogen.sh
    Now, to compile Siril one can do:
    ./autogen.sh --prefix=......
    make
    make install

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+(
+  cd "$srcdir" &&
+  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
