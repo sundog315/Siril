a60953be8b99152ac33fda95188213fe340b085e
lock <lock@0131fb5d-c1a1-4c56-9934-cd381e373ad5>
2016-11-10 11:02:55 +0000

    Minor fix in menus

--- src/gui/callbacks.c
+++ src/gui/callbacks.c
@@ -1581,6 +1581,7 @@ static void do_popup_graymenu(GtkWidget *my_widget, GdkEventButton *event) {
 	gtk_widget_set_sensitive(lookup_widget("undo_item1"), is_undo_available());
 	gtk_widget_set_sensitive(lookup_widget("redo_item1"), is_redo_available());
 	gtk_widget_set_sensitive(lookup_widget("menu_gray_psf"), selected);
+	gtk_widget_set_sensitive(lookup_widget("menu_gray_seqpsf"), selected);
 	gtk_widget_set_sensitive(lookup_widget("menu_gray_pick_star"), selected);
 	gtk_widget_set_sensitive(lookup_widget("menu_gray_crop"), selected && is_a_single_image_loaded);
 	gtk_widget_set_sensitive(lookup_widget("menu_gray_crop_seq"), selected && sequence_is_loaded());
@@ -1831,7 +1832,11 @@ void update_MenuItem() {
 	any_RGB_image_is_loaded = isrgb(&gfit) && (single_image_is_loaded() || sequence_is_loaded());
 
 	/* File Menu */
-	gtk_widget_set_sensitive(lookup_widget("save1"), any_image_is_loaded);
+	gtk_widget_set_sensitive(lookup_widget("menu_save_fits"), any_image_is_loaded);
+	gtk_widget_set_sensitive(lookup_widget("menu_save_tiff"), any_image_is_loaded);
+	gtk_widget_set_sensitive(lookup_widget("menu_save_bmp"), any_image_is_loaded);
+	gtk_widget_set_sensitive(lookup_widget("menu_save_jpg"), any_image_is_loaded);
+	gtk_widget_set_sensitive(lookup_widget("menu_save_pbm"), any_image_is_loaded);
 	gtk_widget_set_sensitive(lookup_widget("menu_FITS_header"), any_image_is_loaded && gfit.header != NULL);
 
 	/* Edit Menu */
